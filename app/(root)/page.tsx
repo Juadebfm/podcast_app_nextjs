93d3da3c9e380f6b622327a49c7f8a34efbe5ee3
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-06-19 12:57:00 +0100

    fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,6 +13,7 @@ const Home = () => {
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcast</h1>
         <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+          {/* There is an issue here */}
           {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
         </div>
         <div className="podcast_grid">
